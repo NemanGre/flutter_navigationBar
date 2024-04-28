01fdba58308aa192203ff7d769a5f8a7f0b3e54a
Neman GRE <138532061+NemanGre@users.noreply.github.com>
2024-04-28 10:54:09 +0430

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'animated button',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
